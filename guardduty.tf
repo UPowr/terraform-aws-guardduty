db22c3934fe1c08dea811cabfd3bd96be2bcd5ab
Jon Seymour <jon.seymour@gmail.com>
2023-08-10 22:41:29 +1000

    disable invites

--- guardduty.tf
+++ guardduty.tf
@@ -2,7 +2,7 @@ resource "aws_guardduty_detector" "member" {
   enable = true
 }
 
-resource "aws_guardduty_invite_accepter" "member" {
-  detector_id       = aws_guardduty_detector.member.id
-  master_account_id = var.admin_account_id
-}
+# resource "aws_guardduty_invite_accepter" "member" {
+#  detector_id       = aws_guardduty_detector.member.id
+#  master_account_id = var.admin_account_id
+# }
